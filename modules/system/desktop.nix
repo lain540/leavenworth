ff4ca4d05b1e1ce039d7db616c1f8fe95b5b8bcb
Svea <mycckccvvcecnmvhkchk@gmail.com>
2026-02-15 17:13:01 +0100

    Add files via upload

---
+++ modules/system/desktop.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # Hyprland - system-level enablement
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    xwayland.enable = true;
+  };
+
+  # tuigreet - minimal login manager
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
+
+  # Fonts - Terminus Nerd Font
+  fonts.packages = with pkgs; [
+    terminus-nerdfont
+  ];
+
+  # Portals for screen sharing, file pickers, etc.
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+
+  # Enable sound with PipeWire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+}
