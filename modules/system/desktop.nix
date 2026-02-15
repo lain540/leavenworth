eda063da09184baaaca653228fa684720bf3a112
Svea <mycckccvvcecnmvhkchk@gmail.com>
2026-02-15 19:28:42 +0100

    Update desktop.nix

--- modules/system/desktop.nix
+++ modules/system/desktop.nix
@@ -13,7 +13,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
         user = "greeter";
       };
     };
